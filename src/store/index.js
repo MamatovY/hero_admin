1369e1860c891ddef14059af6e32b5bb81fb8998
MamatovY <mamatovyiman28@gmail.com>
2023-04-03 16:59:54 +0600

    add middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,20 @@
-import { createStore, combineReducers } from 'redux';
-import { filters, heroes } from '../reducers';
+import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware } from 'redux'
+import ReduxThunk from 'redux-thunk'
+import { filters, heroes } from '../reducers'
 
-const store = createStore(combineReducers({ filters, heroes })
-    , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const stringMiddleware = (store) => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({ type: action })
+    }
+    return next(action)
+}
+console.log(ReduxThunk);
+const store = createStore(
+    combineReducers({ heroes, filters }),
+    compose(applyMiddleware(ReduxThunk, stringMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+)
 
-export default store;
+export default store
+
